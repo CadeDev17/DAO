41a27594fff0e4cc83ca5ef76f375b6b6169f4c4
Decryptr22 <decryptr22@gmail.com>
2023-02-20 19:02:55 -0500

    Initial Commit

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x98408F3281960C83c03a72d0Edc4e6A1C7756F70";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
